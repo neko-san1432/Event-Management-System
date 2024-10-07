1f7d299c8adac5e2abce4c0201976947e43c8107
Pyrrhus Elcid Go <pyrrhuselcidgo@gmail.com>
2024-10-07 17:23:45 +0800

    Delete grpc-context-1.27.2.jar

--- src/main/java/com/Pyrrhus Go/Event Management Calendar and Scheduler/services/NumberGenerator.java
+++
@@ -1,5 +0,0 @@
-public class NumberGenerator {
-  public int randomizer(){
-    return (int) (Math.random()*999999);
-  }
-}
--- src/main/java/com/Pyrrhus Go/Event Management Calendar and Scheduler/utils/Run.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Run {
-  public static void main(String[] args) {
-
-    SwingUtilities.invokeLater(()->new LoginFrame().setVisible(true));
-  }
-}
